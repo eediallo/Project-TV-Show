49abe61212ae3f018ae71c4b65fd3957f8090ad7
abdoul42 <elhhabdouldiallo@gmail.com>
2024-11-19 14:30:33 +0000

    format season and episode

--- script.js
+++ script.js
@@ -7,13 +7,21 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 }
 
+function formatSeason(season){
+  return season  < 10 ? `S0${season}` : `S${season}`
+}
+
+function formatEpisode(episode){
+  return episode  < 10 ? `E0${episode}` : `E${episode}`
+}
+
 const oneEpisode = getOneEpisode();
 
 function createEpisodeCard(){
   const episodeCard = document.querySelector('#episode-card-template').content.cloneNode(true)
   episodeCard.querySelector('h3').textContent = oneEpisode.name
-  episodeCard.querySelector('[data-season-number]').textContent = oneEpisode.season
-  episodeCard.querySelector('[data-episode-number]').textContent = oneEpisode.number
+  episodeCard.querySelector('[data-season-number]').textContent = formatSeason(oneEpisode.season)
+  episodeCard.querySelector('[data-episode-number]').textContent = formatEpisode(oneEpisode.number)
   episodeCard.querySelector('img').setAttribute('src', oneEpisode.image.medium)
   episodeCard.querySelector('p').textContent = oneEpisode.summary.replace(/<p>|<\/p>/g, '')
   return episodeCard  
