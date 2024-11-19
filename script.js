b16e7a52e418f1fdaff3f2bdd9336e85a8e10aa4
abdoul42 <elhhabdouldiallo@gmail.com>
2024-11-19 14:24:56 +0000

    creates an episode card

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
+const oneEpisode = getOneEpisode();
+
+function createEpisodeCard(){
+  const episodeCard = document.querySelector('#episode-card-template').content.cloneNode(true)
+  episodeCard.querySelector('h3').textContent = oneEpisode.name
+  episodeCard.querySelector('[data-season-number]').textContent = oneEpisode.season
+  episodeCard.querySelector('[data-episode-number]').textContent = oneEpisode.number
+  episodeCard.querySelector('img').setAttribute('src', oneEpisode.image.medium)
+  episodeCard.querySelector('p').textContent = oneEpisode.summary.replace(/<p>|<\/p>/g, '')
+  return episodeCard  
+}
+
+const rootElem = document.getElementById("root");
+const episodeCard = createEpisodeCard()
+rootElem.appendChild(episodeCard)
+
+
+
+
+console.log(episodeCard)
 window.onload = setup;
