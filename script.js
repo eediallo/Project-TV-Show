b7b932ac0f02004a6abce93337e3e7c8f5b04e59
Anna Fedyna <annafedyna03@gmail.com>
2024-12-08 00:24:20 +0000

    added  functionality for episode selector dropdown

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const input = document.getElementById("search-input");
+const displayNumber = document.getElementById("display-number");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
   searchEpisodeCards();
+  createEpisodeDropDownSelector(allEpisodes);
 }
 
 function makePageForEpisodes(allEpisodes) {
@@ -37,28 +41,62 @@ function createEpisodeCard(episode) {
 }
 
 function searchEpisodeCards() {
-  const input = document.getElementById("search-input");
-  const cards = document.querySelectorAll(".episode-card");
-  const displayNumber = document.getElementById("display-number");
-
   input.addEventListener("input", (e) => {
-    const searchText = e.target.value.toLowerCase();
-    let countShownEpisodes = 0;
-
-    cards.forEach((card) => {
-      const cardText = card.textContent.toLowerCase();
-
-      if (cardText.includes(searchText)) {
-        card.style.display = "block";
-        countShownEpisodes += 1;
-      } else {
-        card.style.display = "none";
-      }
-    });
-
-    displayNumber.textContent = `${countShownEpisodes} / 73 episode${
-      countShownEpisodes !== 1 ? "s" : ""
-    } `;
+    renderSelectedEpisodes(e);
   });
 }
+
+function renderSelectedEpisodes(e) {
+  const searchText = e.target.value.toLowerCase();
+  let countShownEpisodes = 0;
+
+  const cards = document.querySelectorAll(".episode-card");
+
+  cards.forEach((card) => {
+    const cardText = card.textContent.toLowerCase();
+
+    if (cardText.includes(searchText)) {
+      card.style.display = "block";
+      countShownEpisodes += 1;
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  displayNumber.textContent = `${countShownEpisodes} / 73 episode${
+    countShownEpisodes !== 1 ? "s" : ""
+  } `;
+}
+
+function createEpisodeDropDownSelector(allEpisodes) {
+  const episodeSelectorTemplate = document.querySelector(
+    "#episode-selector-temp"
+  );
+  const episodeSelectorTemplateClone =
+    episodeSelectorTemplate.content.cloneNode(true);
+  document.body.insertBefore(
+    episodeSelectorTemplateClone,
+    document.querySelector(".search-panel")
+  );
+
+  const episodeSelector = document.querySelector("#episode-selector");
+
+  episodeSelector.append(
+    ...allEpisodes.map((episode) => {
+      const episodeOption = document.createElement("option");
+      episodeOption.value = episode.name;
+      const formattedSeason = `S${formatSeason(episode.season)}`;
+      const formattedEpisode = `E${formatSeason(episode.number)}`;
+      const episodeName = episode.name;
+      episodeOption.textContent = `${formattedSeason}${formattedEpisode} - ${episodeName}`;
+      episodeOption.classList.add("episode-option");
+
+      return episodeOption;
+    })
+  );
+  episodeSelector.addEventListener("change", (event) => {
+    renderSelectedEpisodes(event);
+  });
+}
+
 window.onload = setup;
