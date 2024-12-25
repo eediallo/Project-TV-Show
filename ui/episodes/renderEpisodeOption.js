56507b462f44ad3d02979c8000dcb49ab3d39c86
eediallo <elhhabdouldiallo@gmail.com>
2024-12-25 21:51:06 +0000

    change option value

--- ui/episodes/renderEpisodeOption.js
+++ ui/episodes/renderEpisodeOption.js
@@ -4,7 +4,7 @@ function renderEpisodeOptions(episodes) {
   const episodeSelector = document.getElementById("episode-selector");
   episodeSelector.innerHTML = "";
   const allOption = document.createElement("option");
-  allOption.value = "all-episode";
+  allOption.value = "all-episodes";
   allOption.textContent = "All Episodes";
   episodeSelector.appendChild(allOption);
 
