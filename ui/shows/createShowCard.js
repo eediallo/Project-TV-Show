fc6c45b1d8d489c94af7c8371ddf5c204bdab552
eediallo <elhhabdouldiallo@gmail.com>
2024-12-25 22:17:53 +0000

    update create show card

--- ui/shows/createShowCard.js
+++ ui/shows/createShowCard.js
@@ -1,11 +1,33 @@
 function createShowCard(show) {
+  console.log(show);
   const showCard = document.createElement("section");
+  showCard.classList.add("show-card");
+
+  const header = document.createElement("div");
+  header.classList.add("header");
+
   const name = document.createElement("h1");
   name.textContent = show.name;
+
+  header.append(name);
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+
+  const imgDiv = document.createElement("div");
+
   const img = new Image();
   img.setAttribute("src", show.image.medium);
+
   const summary = document.createElement("p");
-  summary.innerHTML = show.summary.replace('/<p>/g', '');
+  summary.classList.add("summary");
+  summary.innerHTML = show.summary.replace("/<p>/g", "");
+
+  const genresStatusRuntimeRating = document.createElement("div");
+  genresStatusRuntimeRating.classList.add("details");
+
+  const rating = document.createElement("p");
+  rating.textContent = `Rating: ${show.rating.average}`;
 
   const genres = document.createElement("p");
   genres.textContent = `Genres: ${show.genres[0]} | ${show.genres[1]} `;
@@ -16,7 +38,11 @@ function createShowCard(show) {
   const runtime = document.createElement("p");
   runtime.textContent = `Runtime: ${show.runtime}`;
 
-  showCard.append(name, img, summary, genres, status, runtime);
+  imgDiv.append(img);
+  genresStatusRuntimeRating.append(rating, genres, status, runtime);
+
+  content.append(imgDiv, summary, genresStatusRuntimeRating);
+  showCard.append(header, content);
 
   return showCard;
 }
