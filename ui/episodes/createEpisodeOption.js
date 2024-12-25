f94ee91c3ad4755660a79eeb700866bca51359d7
eediallo <elhhabdouldiallo@gmail.com>
2024-12-25 20:22:53 +0000

    create class for episode options

--- ui/episodes/createEpisodeOption.js
+++ ui/episodes/createEpisodeOption.js
@@ -2,6 +2,7 @@ import { formatSeasonEpisode } from "../formatSeasonEpi.js";
 
 function createEpisodeOption(episode) {
   const episodeOption = document.createElement("option");
+  episodeOption.classList.add('episode-option')
   episodeOption.value = episode.name;
   const formattedSeason = formatSeasonEpisode(episode.season, "season");
   const formattedEpisode = formatSeasonEpisode(episode.number);
