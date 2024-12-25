a251c8b29365459a13d844b2b8990a4a919808f2
eediallo <elhhabdouldiallo@gmail.com>
2024-12-25 15:01:39 +0000

    hide episode drop down when page first loads

--- ui/domElements.js
+++ ui/domElements.js
@@ -3,6 +3,8 @@ const displayNumber = document.getElementById("display-number");
 const episodeSelector = document.getElementById("episode-selector");
 const showSeletor = document.getElementById("show-selector");
 const rootElement = document.getElementById("root");
+const episodeDropDown = document.querySelector("#episode-drop-down");
+episodeDropDown.style.display = "none";
 
 export {
   searchInput,
