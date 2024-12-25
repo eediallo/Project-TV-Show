92465c243ed2857c9c062f0d110659967af02c48
eediallo <elhhabdouldiallo@gmail.com>
2024-12-25 17:12:09 +0000

    refactor filter

--- ui/searchFilter.js
+++ ui/searchFilter.js
@@ -1,5 +1,5 @@
 import { render } from "./render.js";
-import { searchInput, episodeSelector, displayNumber, episodeDropDown } from "./domElements.js";
+import { searchInput, displayNumber, episodeDropDown } from "./domElements.js";
 import { state } from "../data/state.js";
 import { createShowCard } from "./shows/createShowCard.js";
 import { createEpisodeCard } from "./episodes/createEpisodeCard.js";
@@ -7,26 +7,40 @@ import { createEpisodeCard } from "./episodes/createEpisodeCard.js";
 function handleSearchAndFilter() {
   const searchTerm = searchInput.value.toLowerCase();
   let filteredItems = [];
+  let itemType = "";
 
   if (episodeDropDown.style.display === "none") {
-    // Filtering shows
-    filteredItems = state.allShows.filter((show) => {
-      const showText = `${show.name} ${show.summary}`.toLowerCase();
-      return showText.includes(searchTerm);
-    });
-    render(filteredItems, createShowCard);
+    filterShows(filteredItems, state.allShows, searchTerm, itemType);
   } else {
-    // Filtering episodes
-    filteredItems = state.allEpisodes.filter((episode) => {
-      const episodeText = `${episode.name} ${episode.summary}`.toLowerCase();
-      return episodeText.includes(searchTerm);
-    });
-    render(filteredItems, createEpisodeCard);
+    filterEpisodes(filteredItems, state.allEpisodes, searchTerm, itemType);
   }
 
   displayNumber.textContent = `${filteredItems.length} / ${
-    episodeDropDown.style.display === "none" ? state.allShows.length : state.allEpisodes.length
-  } item${filteredItems.length !== 1 ? "s" : ""}`;
+    episodeDropDown.style.display === "none"
+      ? state.allShows.length
+      : state.allEpisodes.length
+  } ${itemType}${filteredItems.length !== 1 ? "s" : ""}`;
+}
+
+function filterShows(filteredItems, shows, searchTerm, itemType) {
+  filteredItems = shows.filter((show) => {
+    const showText = `${show.name} ${show.summary}`.toLowerCase();
+    return showText.includes(searchTerm);
+  });
+  render(filteredItems, createShowCard);
+  itemType = "show";
+  document.querySelector(".display-label").textContent = "Displaying all shows";
+}
+
+function filterEpisodes(filteredItems, episodes, searchTerm, itemType) {
+  filteredItems = episodes.filter((episode) => {
+    const episodeText = `${episode.name} ${episode.summary}`.toLowerCase();
+    return episodeText.includes(searchTerm);
+  });
+  render(filteredItems, createEpisodeCard);
+  itemType = "episode";
+  document.querySelector(".display-label").textContent =
+    "Displaying all episodes";
 }
 
 export { handleSearchAndFilter };
