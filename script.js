c93a3c030b6e19db92d09eda0ef39aeeae25ddff
eediallo <elhhabdouldiallo@gmail.com>
2024-12-26 12:09:23 +0000

    gets rid of unused import

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 import { getShows } from "./data/getShows.js";
-// import {
-//   searchInput,
-//   episodeSelector,
-//   showSelector,
-//   episodeDropDown,
-//   showDropDown,
-//   backToShowsBtn,
-// } from "./ui/domElements.js";
 import { state } from "./data/state.js";
 import { renderShowsOptions } from "./ui/shows/showOption.js";
 import { render } from "./ui/render.js";
